9fd4c1b6352c01612eceba40a28d2afef9bb1de3
Trista Runions <kittysnyper@gmail.com>
2023-05-09 09:36:27 -0700

    finish project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TOP-Etch-a-Sketch
 
-In this project, I will creat an Etch-a-Sketch using javascript. This project will demonstrate my knowledge of DOM manipulation.
+In this project, I will creat an Etch-a-Sketch using javascript. This project will demonstrate my knowledge of DOM manipulation and CSS flexbox and grid.
--- script.js
+++ script.js
@@ -4,43 +4,65 @@ const btnBlack = document.createElement('button')
 const btnRGB = document.createElement('button')
 const btnReset = document.createElement('button')
 
+const createDiv = (size) => {
+    container.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`
 
-const createDiv = (col, rows) => {
-    for (let i = 0; i < (col * rows); i++) {
+    for (let i = 0; i < (size * size); i++) {
         const div = document.createElement('div')
-        container.style.gridTemplate = `repeat(${col}, 1fr) / repeat(${rows}, 1fr)`
         container.appendChild(div).classList.add('box')
 
 
     }
 }
-createDiv(16,16)
-const gridSize = () => {
-    const size = prompt('How big of a canvas do you want?', '')
-    promptNum = Number(size)
-    createDiv(size,size)
-}
 
-
-const blackColor = () => {
+const black = () => {
     const boxs = container.querySelectorAll('.box')
-    btnBlack.textContent = 'Black'
-    btnBlack.addEventListener('click', () => {
-        boxs.forEach((box) => {
+    boxs.forEach((box) => {
             box.addEventListener('mouseover', () => {
                 box.style.background = 'black'
             })
         })
+}
+
+const gridSize = () => {
+    const size = prompt('How big of a canvas do you want?', '')
+    promptNum = Number(size)
+    createDiv(size)
+    if (size > 100){
+        alert('Canvas is to big')
+    }
+}
+
+
+const blackBTN = () => {
+    
+    btnBlack.textContent = 'Black'
+    btnBlack.addEventListener('mousedown', () => {
+        btnBlack.style.backgroundColor = 'black'
+        btnBlack.style.color = 'white'
     })
+    btnBlack.addEventListener('mouseup' , () => {
+        btnBlack.style.backgroundColor = 'white'
+        btnBlack.style.color = 'black'
+    })
+    btnBlack.addEventListener('click',black)
 
     btnContainer.appendChild(btnBlack).classList.add('btn')
 }
 
-blackColor()
 
-const rainbowColor = () => {
+
+const rainbowBTN = () => {
     const boxs = container.querySelectorAll('.box')
     btnRGB.textContent = 'Rainbow'
+    btnRGB.addEventListener('mousedown', () => {
+        btnRGB.style.backgroundColor = 'black'
+        btnRGB.style.color = 'white'
+    })
+    btnRGB.addEventListener('mouseup' , () => {
+        btnRGB.style.backgroundColor = 'white'
+        btnRGB.style.color = 'black'
+    })
     btnRGB.addEventListener('click', () => {
         boxs.forEach((box) => {
             box.addEventListener('mouseover', () => {
@@ -53,16 +75,31 @@ const rainbowColor = () => {
     btnContainer.appendChild(btnRGB).classList.add('btn')
 }
 
-rainbowColor()
+
 
 const resetBTN = () => {
     const boxs = container.querySelectorAll('.box')
     btnReset.textContent = 'Reset'
+    btnReset.addEventListener('mousedown', () => {
+        btnReset.style.backgroundColor = 'black'
+        btnReset.style.color = 'white'
+    })
+    btnReset.addEventListener('mouseup' , () => {
+        btnReset.style.backgroundColor = 'white'
+        btnReset.style.color = 'black'
+    })
     btnReset.addEventListener('click', () => {
-        boxs.forEach(box => box.style.background = '')
+        gridSize()
+        boxs.forEach(box => {
+            box.style.background = ''
+        })
+        
         
     })
     btnContainer.appendChild(btnReset).classList.add('btn')
 }
 
+createDiv(16,16)
+blackBTN()
+rainbowBTN()
 resetBTN()
